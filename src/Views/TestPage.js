eb888901e691a48b8f7f46f48901d6bfc25f6f96
pinkishincoloragain <smb1103@gmail.com>
2022-02-26 05:32:43 +0000

    adding test page

--- src/Views/TestPage.js
+++ src/Views/TestPage.js
@@ -2,11 +2,24 @@ import { Box } from "@mui/system";
 
 export default function TestPage() {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Box sx={{ width: "100vw", height: "10vh", backgroundColor: "#1f1f1f" }}>
-        asdf
-      </Box>
-      <Box></Box>
+    <Box sx={{ ml: "10vw", display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          mt: "2vh",
+          width: "80vw",
+          height: "8vh",
+          backgroundColor: "#1f1f1f",
+        }}
+      ></Box>
+      <Box
+        sx={{
+          mt: "10vh",
+          width: "80vw",
+          height: "70vh",
+          borderRadius: "10px",
+          boxShadow: "1px 1px 10px #1f1f1f",
+        }}
+      ></Box>
     </Box>
   );
 }
